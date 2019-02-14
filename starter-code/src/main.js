c2ca141d9c307c9da5363e61915ddbef784cfba8
Guido <Guido@mac.local>
2019-02-14 21:31:21 +0100

    work in progress

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -25,8 +25,10 @@ var cards = [
   { name: 'thor',            img: 'thor.jpg' }
 ];
 
+var memoryGame = new MemoryGame(cards);
+
 $(document).ready(function(){
-  var memoryGame = new MemoryGame(cards);
+  
   var html = '';
   memoryGame.cards.forEach(function (pic) {
     html += '<div class="card" data-card-name="'+ pic.name +'">';
@@ -37,11 +39,39 @@ $(document).ready(function(){
 
   // Add all the div's to the HTML
   $('#memory_board').html(html);
-
+  
   // Bind the click event of each element to a function
   $('.back').click(function () {
-    // TODO: write some code here
+      showCard( $(this).attr("name") );
+      memoryGame.cardsFlipped++;
+      
+
+      if (memoryGame.cardsFlipped == 2) {
+        hideCard( $(this).attr("name") );
+        hideCard(memoryGame.firstFlipped);
+        memoryGame.cardsFlipped = 0
+      } else {
+         memoryGame.firstFlipped = $(this).attr("name");
+         
+      }
+
   });
+
+  
 });
+
+function showCard(card) {
+    $(`[name="${card}"]`).toggleClass('front');
+    $(`[name="${card}"]`).toggleClass('back');
+    $(`[name="${card}"]`).next().toggleClass('front');
+    $(`[name="${card}"]`).next().toggleClass('back');
+}
+
+function hideCard(card) {
+     $(`[name="${card}"]`).toggleClass('front');
+     $(`[name="${card}"]`).toggleClass('back');
+     $(`[name="${card}"]`).next().toggleClass('front');
+     $(`[name="${card}"]`).next().toggleClass('back');
+}
 
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,34 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+  this.cardsFlipped = 0;
+  this.firstFlipped = {};
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+     
 };
 
-MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {  
+  pairsClicked++;
+  if( firstCard == secondCard) {
+    pairsGuessed++;
+    return true
+  } else {
+    return false 
+  }
 }
 
+
 MemoryGame.prototype.isFinished = function () {
+  return (this.pairsGuessed == 12) ? true : false  
 };
+
+
+
+
+
+
